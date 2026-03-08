da659bc289ee68f53e91fa43ff5a6256a583aa51
laala <hamdaouilaala2@gmail.com>
2026-03-08 10:52:40 -0400

    creation de la clase parc et l'ajout des atributes

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ class voiture:
         self.marque = marque
         self.modele = modele
         self.imatriculation = imatriculation
+    def affiche(self):
+        return f"{self.marque} {self.modele} {self.imatriculation}"
+
+class parc:
+    def __init__(self,capacite):
+        self.capacite = capacite
+        self.voiture = []
